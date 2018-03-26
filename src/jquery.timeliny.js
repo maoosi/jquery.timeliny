25ebf12d80c6c882317d114b497ea74ba7219056
jtloong <joshua.t.loong@gmail.com>
2018-03-26 14:41:34 -0400

    Added ability to control from arrowkeys instead of just clicks - only works for chronological years

--- src/jquery.timeliny.js
+++ src/jquery.timeliny.js
@@ -35,6 +35,7 @@
 			_createDots();
 			_fixBlockSizes();
 			_clickBehavior();
+      _arrowBehavior();
 			_createVerticalLine();
 			_updateTimelinePos();
 			_resizeBehavior();
@@ -194,6 +195,26 @@
 
 				return false;
 			});
+		}
+
+    function _arrowBehavior() {
+      $('html').keydown(function(e){
+
+       if(e.which == 39){
+         var years = $(this).find('.' + options.className + '-timeblock:not(.inactive) .' + options.className + '-dot');
+         var currYear =$(years).parent().parent().find('.' + options.className + '-timeblock.active').attr('data-year');
+         var nextYear = Number(currYear) + 1;
+         goToYear(String(nextYear));
+       }
+       else if (e.which == 37){
+         var years = $(this).find('.' + options.className + '-timeblock:not(.inactive) .' + options.className + '-dot');
+         var currYear =$(years).parent().parent().find('.' + options.className + '-timeblock.active').attr('data-year');
+         var nextYear = Number(currYear) - 1;
+         goToYear(String(nextYear));
+       }
+
+
+      });
 		}
 
 		/**
